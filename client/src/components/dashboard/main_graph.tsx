cbaabb4ac551fece8a85db53dea4192310a1c700
Byeonggyu Shin <94808683+byeonggyu-shin@users.noreply.github.com>
2023-06-10 11:13:29 +0900

    refactor: separate API call from graph component

--- client/src/components/dashboard/main_graph.tsx
+++ client/src/components/dashboard/main_graph.tsx
@@ -1,79 +1,77 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import * as echarts from "echarts";
 import axios from "axios";
+import { Main_graph_Api } from "@/axios/dashBoardApi";
 
 const Main_graph = () => {
   const chartRef = useRef();
+  const [graphData, setGraphData] = useState();
 
   useEffect(() => {
-    if (chartRef.current) {
-      var myChart = echarts.init(chartRef.current);
-      myChart.showLoading();
+    const fetchData = async () => {
+      if (chartRef.current) {
+        let myChart = echarts.init(chartRef.current);
+        myChart.showLoading();
 
-      axios
-        .get("http://localhost:4000/graph")
-        .then((response) => {
-          const graph = response.data;
+        const graph: any = await Main_graph_Api();
+        myChart.hideLoading();
 
-          myChart.hideLoading();
-
-          graph.nodes.forEach((node) => {
-            node.label = {
-              show: node.symbolSize > 30,
-            };
-          });
-
-          const option = {
-            title: {
-              text: "Les Miserables",
-              subtext: "Default layout",
-              top: "bottom",
-              left: "right",
-            },
-            tooltip: {},
-            legend: [
-              {
-                data: graph.categories.map((a) => a.name),
-              },
-            ],
-            animationDuration: 1500,
-            animationEasingUpdate: "quinticInOut",
-            series: [
-              {
-                name: "Les Miserables",
-                type: "graph",
-                layout: "none",
-                data: graph.nodes,
-                links: graph.links,
-                categories: graph.categories,
-                roam: true,
-                label: {
-                  position: "right",
-                  formatter: "{b}",
-                },
-                lineStyle: {
-                  color: "source",
-                  curveness: 0.3,
-                },
-                emphasis: {
-                  focus: "adjacency",
-                  lineStyle: {
-                    width: 10,
-                  },
-                },
-              },
-            ],
+        graph.nodes.forEach((node) => {
+          node.label = {
+            show: node.symbolSize > 30,
           };
-
-          myChart.setOption(option);
-        })
-        .catch((error: any) => {
-          console.log(error);
         });
-    }
+
+        const option: any = {
+          title: {
+            text: "Les Miserables",
+            subtext: "Default layout",
+            top: "bottom",
+            left: "right",
+          },
+          tooltip: {},
+          legend: [
+            {
+              data: graph.categories.map((a) => a.name),
+            },
+          ],
+          animationDuration: 1500,
+          animationEasingUpdate: "quinticInOut",
+          series: [
+            {
+              name: "Les Miserables",
+              type: "graph",
+              layout: "none",
+              data: graph.nodes,
+              links: graph.links,
+              categories: graph.categories,
+              roam: false,
+              label: {
+                position: "right",
+                formatter: "{b}",
+              },
+              lineStyle: {
+                color: "source",
+                curveness: 0.3,
+              },
+              emphasis: {
+                focus: "adjacency",
+                lineStyle: {
+                  width: 10,
+                },
+              },
+            },
+          ],
+        };
+
+        myChart.setOption(option);
+      }
+    };
+
+    fetchData();
   }, []);
 
-  return <div className="main_graph"  ref={chartRef} style={{ width: "100%", height: "100vh" }}></div>;
+  return <div className="w-full h-screen" ref={chartRef}></div>;
 };
 
 export default Main_graph;
