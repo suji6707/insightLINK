36186f9aae31090e51a59585243e1a3b131f13e0
Byeonggyu Shin <94808683+byeonggyu-shin@users.noreply.github.com>
2023-06-10 14:51:50 +0900

    feat: initialize UserPanel component

---
+++ client/src/components/dashboard/UseInfo.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function UserInfo() {
+
+  
+
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+
--- client/src/pages/dashboard.tsx
+++ client/src/pages/dashboard.tsx
@@ -1,12 +1,14 @@
 import React, { useRef } from "react";
 // Component
 import NavBar from "../components/NavBar";
+import UserInfo from '../components/dashboard/UseInfo';
 import ImgUploadBtn from "../components/ImgUploadBtn";
 
 export default function Dashboard() {
   return (
     <div>
       <NavBar />
+      <UserInfo />
       <ImgUploadBtn />
     </div>
   );
