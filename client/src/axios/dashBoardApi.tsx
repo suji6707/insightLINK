16836d3af30593fab6c28413b0733f48da7c8b37
Byeonggyu Shin <94808683+byeonggyu-shin@users.noreply.github.com>
2023-06-10 14:51:50 +0900

    feat: initialize UserPanel component

--- client/src/axios/dashBoardApi.tsx
+++ client/src/axios/dashBoardApi.tsx
@@ -1,11 +1,20 @@
 import axios from "axios";
-const api = "http://localhost:4000/";
+const api = "http://localhost:4000";
 
 export const Main_graph_Api = async () => {
   try {
-    const response = await axios.get(`${api}graph`);
+    const response = await axios.get(`${api}/graph`);
     const graph = response.data;
     return graph;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const User_Info_Api = async (params: number | undefined) => {
+  try {
+    const response = await axios.get(`${api}/member`);
+    // const response = await axios.get(`${api}/member/${params}`);
   } catch (error) {
     console.log(error);
   }
