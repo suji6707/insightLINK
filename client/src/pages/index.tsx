ae402654ca5adfba889245d60341a190fe695ae8
godee95 <50854903+godee95@users.noreply.github.com>
2023-06-13 18:34:54 +0900

    feat: Add front back signup - #21

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,11 +1,19 @@
 import React, { useRef } from "react";
 // Component
 import NavBar from "../components/NavBar";
+import LoginButton from "../components/LoginButton";
 
 export default function Home() {
   return (
     <div>
       <NavBar />
+      <div className="flex justify-center mt-20">
+        <div className="text-center">
+          <h3 className="text-4xl font-bold mb-5">insightLINK</h3>
+          <p className="text-lg mb-20">갤러리 속 인사이트를 이어주는 아카이브</p>
+          <LoginButton />
+        </div>
+      </div>
     </div>
   );
 }
--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,15 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 // Component
 import NavBar from "../components/NavBar";
 import LoginButton from "../components/LoginButton";
+import { Button, Divider, Input } from 'antd';
 
 export default function Home() {
+
+  const [userEmail, setUserEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   useEffect(() => {
     async function loadGapi() {
       const { gapi } = await import('gapi-script');
 
-      const clientId = process.env.GOOGLE_CLIENT_ID;
-    //   const clientId = '862985060268-bns768k2p01btrjkdk94f8hkrvqlt5d8.apps.googleusercontent.com';
+      // const clientId = process.env.GOOGLE_CLIENT_ID;
+      const clientId = '862985060268-bns768k2p01btrjkdk94f8hkrvqlt5d8.apps.googleusercontent.com';
 
       if (gapi && gapi.client) {
         await gapi.client.init({
@@ -25,7 +30,37 @@ export default function Home() {
   return (
     <div>
       <NavBar />
-      <LoginButton />
+        <div className='container mx-auto' style={{ width: '350px' }}>
+          <Divider />
+            <div className="flex flex-col items-center space-y-4">
+                <div className="flex flex-col space-y-4">
+                    <Input
+                        value={userEmail}
+                        onChange={(event) => setUserEmail(event.target.value)}
+                        placeholder="Email"
+                        size="large"
+                        className="form_input"
+                        style={{ width: '300px' }} // Set the width here
+                    />
+                    <Input
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                        placeholder="Password"
+                        size="large"
+                        type="password"
+                        className="form_input"
+                        style={{ width: '300px' }} // Set the width here
+                    />
+                </div>
+            </div>
+            <div className="flex justify-center mt-4">
+            {/* <div className="flex flex-col items-center space-y-4"> */}
+            <Button size="large" className="bg-black text-white font-bold py-2 px-4 rounded mr-2">
+                login
+            </Button>
+            <LoginButton />
+            </div>
+      </div>
     </div>
   );
 }
