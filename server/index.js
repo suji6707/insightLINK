243a99bb6262e88466275d8cb438943560e47b1e
godee95 <50854903+godee95@users.noreply.github.com>
2023-06-13 18:34:54 +0900

    feat: Add front back signup - #21

--- server/index.js
+++ server/index.js
@@ -9,11 +9,12 @@ import bodyParser from 'body-parser'; // 요청정보 처리
 // import postRoutes from "./routes/posts.js"   // 내 포스트 보여주는 화면
 import uploadRouter from './routes/uploads.js';
 import loginRouter from './routes/login.js';
+import signupRouter from './routes/signup.js';
 import { authMiddleware } from './middlewares/auth-middleware.js';
 
 const app = express();
 const server = http.createServer(app);
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8800;
 
 
 /* Middleware */
@@ -26,6 +27,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 /* Routing */
 app.use('/api/upload', uploadRouter);
 app.use('/api/login', loginRouter);
+app.use('/api/signup', signupRouter);
 
 /* session management */
 app.get('/api/users/me', authMiddleware, async (req, res) => {
