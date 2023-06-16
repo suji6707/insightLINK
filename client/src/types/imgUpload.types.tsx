dd4863b144ee40535a015a34721c238a164b7caf
SeryeongK <99seryoung@naver.com>
2023-06-16 14:18:20 +0900

    feat: 인터랙션 추가(이동, 크기 점진적으로 병합) - #38

--- client/src/types/imgUpload.types.tsx
+++ client/src/types/imgUpload.types.tsx
@@ -2,3 +2,30 @@ type ImageListProps = {
   imgList: string[];
   deleteImg: (index: number) => void;
 };
+
+// 태그 병합
+type ChartNode = {
+  id: string;
+  name: string;
+  x: number;
+  y: number;
+  symbolSize: number;
+  category: string;
+  itemStyle?: {
+    borderColor: string;
+    borderWidth: number;
+  };
+};
+
+type ChartLink = {
+  source: string;
+  target: string;
+};
+
+type CombineNodesParams = {
+  myChart: any; // 타입 변경 필요
+  nodes: ChartNode[];
+  links: ChartLink[];
+  node1: ChartNode;
+  node2: ChartNode;
+};
