3662500f964250766f7e93ee69fd57c5f768de93
Choi-Seong-Hyeok <cinemay3@daum.net>
2023-06-22 03:26:33 +0900

    feat : 카드삭제 - #114

---
+++ server/middlewares/cardDelete.js
@@ -0,0 +1,28 @@
+import '../dotenv.js';
+import { db } from '../connect.js';
+
+
+export const deleteCard =  async (req, res) => {
+  /* 가져오려는 카드 */
+  const cardId = req.params.card_id;
+  console.log('cardId : ',cardId);
+
+  /* 로그인 유저 */
+  const { user } = res.locals;    
+  const userId = user.user_id;
+  console.log('userId : ',userId);
+
+  let connection = null;
+    try {
+    connection = await db.getConnection();
+    console.log('WHy?');
+    const query = `delete from File where user_id = ${userId} and file_id = ${cardId};`
+    await connection.query(query);
+    connection.release();
+    res.status(200).send('Success');
+    } catch (err) {
+    console.log(err);
+    res.status(500).send('Internal Server Error'); 
+  }
+};
+
