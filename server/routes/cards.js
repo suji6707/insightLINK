3bfa55da1fcab976500f4c3112094e38dbe338b3
Choi-Seong-Hyeok <cinemay3@daum.net>
2023-06-22 02:35:24 +0900

    feat : 카드수정 - #111

--- server/routes/cards.js
+++ server/routes/cards.js
@@ -2,7 +2,9 @@ import express from 'express';
 import { getCardInfos } from '../middlewares/cardInfo.js';
 import { getTagInfos } from '../middlewares/cardTag.js';
 import { getCards } from '../middlewares/cardCopy.js';
+import { updateCard } from '../middlewares/cardUpdate.js';
 import { authMiddleware } from '../middlewares/auth-middleware.js';
+
 
 
 const router = express.Router();
@@ -10,5 +12,5 @@ const router = express.Router();
 router.get('/info', authMiddleware, getCardInfos);    // 카드 상세정보
 router.get('/tag', authMiddleware, getTagInfos);      // 태그 클릭시 관련 카드들 조회
 router.post('/copy', authMiddleware, getCards);       // 카드 드래그앤드롭
-
+router.patch('/update/:card_id',authMiddleware,updateCard);    // 카드 업데이트
 export default router;
