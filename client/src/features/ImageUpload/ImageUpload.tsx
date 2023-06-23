fde3d2801bf1ac9f0dedfb457c0783ade8be0ff6
SeryeongK <99seryoung@naver.com>
2023-06-23 21:37:39 +0900

    refactor: 이미지 S3에 업로드 후 링크 서버에 전송 추가 - #135

--- client/src/features/ImageUpload/ImageUpload.tsx
+++ client/src/features/ImageUpload/ImageUpload.tsx
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from "react";
+import AWS from "aws-sdk";
 import tw from "tailwind-styled-components";
 
 import { POST } from "@/axios/POST";
@@ -7,58 +8,99 @@ import { Wrapper } from "@/styles/wrapper";
 // Assets
 import { FiUploadCloud } from "react-icons/fi";
 import ImageList from "@/features/ImageUpload/ImgList";
+import getToken from "@/axios/getToken";
+
+type ImgInfo = {
+  blob: Blob;
+  name: string;
+  size: number;
+  type: string;
+};
 
 export default function ImageUpload({ setShowImgModal }: any) {
   const fileInputRef = useRef<HTMLInputElement>(null);
-  const listRef = useRef<HTMLElement>(null);
   const modalRef = useRef<HTMLDivElement>(null);
-  const [imgList, setImgList] = useState<string[]>([]);
+  const [imgList, setImgList] = useState<ImgInfo[]>([]);
+  const [imageUrl, setImageUrl] = useState<string[]>();
 
   const handleInput = () => {
     fileInputRef.current?.click();
   };
 
   // image input 받기
-  const handleImgChange = (e: any) => {
+  const handleImgChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const selectedImg = e.target.files;
     if (!selectedImg) {
       e.preventDefault();
       return;
     }
-    const selectedImgArray = [...selectedImg];
+    const selectedImgArray = Array.from(selectedImg).map((file) => {
+      const blob = new Blob([file], { type: file.type });
+      return { name: file.name, type: file.type, size: file.size, blob };
+    });
     setImgList(selectedImgArray);
   };
 
   const deleteImg = (selectedIndex: number) => {
-    const newImgList = imgList.filter((_, index) => index !== selectedIndex); // 선택된 인덱스에 해당하는 값 삭제
+    const newImgList = imgList.filter((_, index) => index !== selectedIndex);
     setImgList(newImgList);
   };
 
-  const POSTImgAuto = async () => {
-    const formData = new FormData();
-    for (let file of imgList) {
-      formData.append("photos", file);
-    }
+  if (process.env.S3_ACCESS_KEY && process.env.S3_SECRET_KEY) {
+    AWS.config.update({
+      credentials: {
+        accessKeyId: process.env.S3_ACCESS_KEY,
+        secretAccessKey: process.env.S3_SECRET_KEY,
+      },
+      region: "ap-northeast-2",
+      signatureVersion: "2023-06-23",
+    });
+  }
 
-    // 로컬 이미지 업로드 API
-    const uploadImg = async () => {
-      const token = localStorage.getItem("token");
-      const result = await POST("upload", formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-          authorization: `Bearer ${token}`,
-        },
+  const uploadImages = async () => {
+    try {
+      const promises = imgList.map((file) => {
+        return new Promise<string>((resolve, reject) => {
+          const { blob } = file;
+          const fileName = `${Date.now()}.${file.name}`;
+          const s3 = new AWS.S3();
+          s3.upload(
+            {
+              Bucket: "sw-jungle-s3",
+              Key: fileName,
+              Body: blob,
+              ContentType: file.type,
+            },
+            (error, data) => {
+              if (error) {
+                reject(error);
+              } else if (data && data.Location) {
+                resolve(data.Location);
+                // 하나씩 받음 console.log("?", data.Location);
+              } else {
+                reject(new Error("이미지 업로드 실패 - S3"));
+              }
+            }
+          );
+        });
       });
-      return result;
-    };
 
-    const result: any = await uploadImg();
-    if (result) {
-      setShowImgModal(false);
+      const uploadedImageUrls = await Promise.all(promises);
+      setImageUrl(uploadedImageUrls);
+
+      const POSTImgLink = async () => {
+        const token = getToken();
+        const result = await POST("upload", { imageUrl: imageUrl }, token);
+        console.log(result);
+      };
+
+      POSTImgLink();
+    } catch (error) {
+      console.error("이미지 업로드 실패", error);
     }
   };
 
-  const modalOutsideClicked = (e: any) => {
+  const modalOutsideClicked = (e: React.MouseEvent<HTMLDivElement>) => {
     if (modalRef.current === e.target) {
       setShowImgModal(false);
     }
@@ -93,7 +135,7 @@ export default function ImageUpload({ setShowImgModal }: any) {
         </div>
         <div className="w-5/12 h-4/6 flex flex-col justify-between">
           <ImageList imgList={imgList} deleteImg={deleteImg} />
-          <ClasBtn onClick={() => POSTImgAuto()}>자동</ClasBtn>
+          <ClasBtn onClick={uploadImages}>자동</ClasBtn>
         </div>
       </Wrapper>
     </div>
