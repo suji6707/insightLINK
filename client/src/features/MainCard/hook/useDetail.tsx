f5e24f806725307257a9dcbbc97589ad309fc2f2
Byeonggyu Shin <94808683+byeonggyu-shin@users.noreply.github.com>
2023-06-24 19:29:19 +0900

    fix: 타인 대시보드 카드패널 에러 수정

--- client/src/features/MainCard/hook/useDetail.tsx
+++ client/src/features/MainCard/hook/useDetail.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 // recoil
 import { useRecoilValue } from "recoil";
 import { ClickedCardDetailAtom } from "@/recoil/atoms/MainGraphAtom";
@@ -10,10 +11,17 @@ import { CardDataDetail } from "@/types/dashborad.types";
 function useDetail() {
   const [data, setData] = useState<CardDataDetail>();
   const cardId = useRecoilValue(ClickedCardDetailAtom);
+  const router = useRouter();
 
   useEffect(() => {
+    if (!router.isReady) return;
+
     const getCardData = async () => {
-      const cardData = await Card_Detail_Api(cardId);
+      const userid = Array.isArray(router.query.userid)
+        ? router.query.userid[0]
+        : router.query.userid;
+
+      const cardData = await Card_Detail_Api(cardId, userid);
       setData(cardData);
     };
     getCardData();
