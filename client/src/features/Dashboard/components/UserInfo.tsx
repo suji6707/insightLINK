8bdcd6856c80a7b5dddbb6411327d2e64464fe27
Byeonggyu Shin <94808683+byeonggyu-shin@users.noreply.github.com>
2023-06-24 20:53:22 +0900

    fix: 빌드 에러 수정

--- client/src/features/Dashboard/components/UserInfo.tsx
+++ client/src/features/Dashboard/components/UserInfo.tsx
@@ -1,19 +1,41 @@
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import { User_Info_Api } from "@/axios/dashBoardApi";
+import {
+  Add_Follow_API,
+  Cancel_Follow_API,
+  User_Info_Api,
+} from "@/axios/dashBoardApi";
 import { UserInfo } from "@/types/dashborad.types";
 
 export default function UserInfo() {
-
   const [userInfo, setUserInfo] = useState<UserInfo>();
   const router = useRouter();
+
+  const handleAddFollow = async () => {
+    const userid = Array.isArray(router.query.userid)
+      ? router.query.userid[0]
+      : router.query.userid;
+
+    const addFriend = await Add_Follow_API(userid);
+    console.log(addFriend);
+    return;
+  };
+
+  const handleCancelFollow = async () => {
+    const userid = Array.isArray(router.query.userid)
+      ? router.query.userid[0]
+      : router.query.userid;
+
+    const CancelFriend = await Cancel_Follow_API(userid);
+    return;
+  };
 
   useEffect(() => {
     const getUserInfoData = async () => {
       const userid = Array.isArray(router.query.userid)
-      ? router.query.userid[0]
-      : router.query.userid;
-      
+        ? router.query.userid[0]
+        : router.query.userid;
+
       const response = await User_Info_Api(userid);
       setUserInfo(response);
       console.log("userInfo data: ", response);
@@ -38,6 +60,9 @@ export default function UserInfo() {
             <div>친구 수</div>
             <div>+{userInfo?.followCnt}</div>
           </div>
+
+          <button onClick={handleAddFollow}>팔로우</button>
+          <button onClick={handleCancelFollow}>언팔로우</button>
         </div>
       </div>
     </>
