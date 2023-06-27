9bc3807018c17b2a5da6ea3516e6f273e3591463
Choi-Seong-Hyeok <cinemay3@daum.net>
2023-06-27 16:50:32 +0900

    fix : 다른사람정보조회 - #180

--- server/middlewares/otherInfo.js
+++ server/middlewares/otherInfo.js
@@ -1,7 +1,6 @@
 import '../dotenv.js';
 import { db } from '../connect.js';
-import { userInfoQuery,followCntQuery,tagCntQuery  } from '../db/userQueries.js';
-
+import {userNameQuery, cardCntQuery,followCntQuery,tagCntQuery  } from '../db/userQueries.js';
 
 export const getOtherInfo = async (req, res) => {
   const otherId = req.params.other_id;
@@ -9,16 +8,19 @@ export const getOtherInfo = async (req, res) => {
   try {
     connection = await db.getConnection();
 
-    const [ result ] = await connection.query(userInfoQuery(otherId));
+    const card_cnt = await connection.query(cardCntQuery(otherId));
+    const user_name = await connection.query(userNameQuery(otherId));
     const follow_cnt = await connection.query(followCntQuery(otherId));
     const tag_cnt = await connection.query(tagCntQuery(otherId));
-    const { userName , cardCnt } = result[0];
+
     let tagCnt = tag_cnt[0][0].tag_count;
+    let cardCnt = card_cnt[0][0]['rowCount'];
+    let userName = user_name[0][0]['user_name'];
 
     const data = {
       userName,
       tagCnt,
-      cardCnt: parseInt(cardCnt),
+      cardCnt,
       followCnt: follow_cnt[0][0]['cnt'], 
     };
     
