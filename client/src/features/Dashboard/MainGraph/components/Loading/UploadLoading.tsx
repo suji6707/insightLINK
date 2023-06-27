75490c85cf0940402ef4602062d95729883814d8
SeryeongK <99seryoung@naver.com>
2023-06-28 00:15:27 +0900

    feat: 로딩창 디자인 수정 - #155

--- client/src/features/Dashboard/MainGraph/components/Loading/UploadLoading.tsx
+++ client/src/features/Dashboard/MainGraph/components/Loading/UploadLoading.tsx
@@ -10,6 +10,7 @@ import NavBar from "@/features/Dashboard/components/NavBar";
 import { Wrapper } from "@/styles/wrapper";
 // Assets
 import { BiLoader } from "react-icons/bi";
+// import { AiOutlineClose } from "react-icons/ai";
 
 const UploadLoading = () => {
   const [imageUrl, setImageUrl] = useRecoilState(UploadedImgAtom);
@@ -19,40 +20,41 @@ const UploadLoading = () => {
   return (
     <div className="bg-gray-100 h-screen">
       <NavBar />
-      <Wrapper>
-        <div
-          className="flex flex-col items-center py-15 px-0 gap-4 self-stretch bg-gray-100"
-          style={{
-            padding: "3.75rem 0",
-          }}
-        >
-          <div className="flex justify-center items-center space-x-3 ">
-            <BiLoader
-              className="text-base leading-none"
-              style={{ fontFamily: "xeicon" }}
-            />
-            <p className="text-gray-900 text-xl font-semibold leading-tight tracking-tighter">
-              분석 중인 스크린샷 {imgNum} ...
-            </p>
+      <Wrapper className="flex flex-col h-full justify-center">
+        <div className="flex flex-col items-center self-stretch bg-gray-100 ">
+          <div
+            className="flex flex-col items-center justify-center w-[52.5rem] pt-[0.75rem] pr-[1.5rem] pb-[1.25rem] pl-[1.5rem] rounded-xl border border-gray-100 bg-white"
+            style={{ boxShadow: "0px 4px 8px 0px rgba(0, 0, 0, 0.16)" }}
+          >
+            <div className="flex h-[3.75rem] justify-start items-center self-stretch">
+              <p className="text-[1.5rem] font-kanit font-semibold leading-normal tracking-tight">
+                분석
+              </p>
+              {/* <AiOutlineClose className="text-gray-400 text-1xl font-xeicon leading-normal" /> */}
+            </div>
+            <div className="flex p-[3.75rem] justify-between items-center self-stretch border-b border-gray-900">
+              <div className="flex items-center justify-center gap-4 w-[14.5rem] h-[14.5rem] flex-shrink-0 rounded-md border border-gray-100 bg-gray-50">
+                <img
+                  src={imageUrl[0]}
+                  className="flex-1 self-stretch bg-lightgray bg-contain bg-no-repeat"
+                />
+              </div>
+              <div className="flex items-start content-start gap-y-[0.5rem] gap-x-[0.25rem] h-[14.5rem] py-[0.5rem] px-1 justify-center flex-wrap">
+                태그들
+              </div>
+              <div className="flex flex-col w-[14.5rem] h-[14.5rem] justify-center shrink-0 text-gray-900 text-center text-2xl font-semibold leading-6 tracking-tighter">
+                추출 태그
+              </div>
+            </div>
+            <div className="flex justify-center items-end h-[2.5rem] gap-[0.25rem] self-stretch rounded-[36px]">
+              <BiLoader className="text-blue-500 text-[1.25rem] font-xeicon leading-normal" />
+              <p className="text-gray-900 text-[0.875rem] font-semibold leading-tight tracking-tighter">
+                {imgNum}개의 스크린샷을 분석중입니다...
+              </p>
+            </div>
           </div>
         </div>
-        <div className="flex flex-col items-center self-stretch bg-gray-100">
-          <div className="flex items-center justify-center w-[60rem] h-[27.5rem] px-[3.75rem] py-[7.5rem] rounded-xl border border-gray-100 bg-white">
-            <div className="flex items-center justify-center gap-4 w-[14.5rem] h-[14.5rem] flex-shrink-0 rounded-md border border-gray-100 bg-gray-50">
-              {/* <img
-                src=""
-                className="flex-1 self-stretch bg-lightgray bg-contain bg-no-repeat"
-              /> */}
-            </div>
-            <div className="flex items-start content-start gap-y-[0.5rem] gap-x-[0.25rem] h-[14.5rem] py-[0.5rem] px-1 justify-center flex-wrap">
-              태그들
-            </div>
-            <div className="flex flex-col w-[14.5rem] h-[14.5rem] justify-center shrink-0 text-gray-900 text-center text-2xl font-semibold leading-6 tracking-tighter">
-              추출 태그
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-col justify-center items-center gap-3 flex-1 self-stretch h-[10.125rem] bg-gray-100">
+        <div className="flex flex-col items-center p-8 gap-2 self-stretch">
           <p className="text-gray-800 text-base font-light leading-6 tracking-tighter">
             갤러리 속 숨겨진 인사이트를 발견하세요
           </p>
