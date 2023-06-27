616f64184afbfcd858de1ba24832ee7a36d6281f
SeryeongK <99seryoung@naver.com>
2023-06-27 16:06:12 +0900

    refactor: setCategories, chart options code spliting - #156

---
+++ client/src/features/MainGraph/components/Graph/setCategories.tsx
@@ -0,0 +1,12 @@
+//  카테고리 빈 객체 생성
+const setCategories = (cnt: number): Array<{ name: string }> => {
+  const categories = [];
+  for (let i = 1; i <= cnt + 1; i++) {
+    const obj = { name: `${i}` };
+    categories.push(obj);
+  }
+  console.log(categories);
+  return categories;
+};
+
+export default setCategories;
