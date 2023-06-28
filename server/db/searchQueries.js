6558e505c1e577c30f3f17593afd2deb9dd4f7bb
godee95 <50854903+godee95@users.noreply.github.com>
2023-06-28 17:54:01 +0900

    feat: Add search card - #189

--- server/db/searchQueries.js
+++ server/db/searchQueries.js
@@ -5,6 +5,14 @@ export const tagList = (userId, keyword) => {
           WHERE f.user_id = ${userId} AND f.content LIKE '%${keyword}%' OR t.tag LIKE '%$${keyword}%';`
 }
 
+export const cardList = (userId, tag) => {
+  return `SELECT f.file_id as id, t.tag, f.img_url as imageUrl, f.content  
+          FROM File f
+          JOIN Tag t ON f.file_id = t.file_id
+          WHERE t.tag = '${tag}'
+            AND f.user_id = ${userId};`
+}
+
 export const searchContent = (keyword) => { 
   return `SELECT f.file_id, f.content, t.tag  
             FROM File f
