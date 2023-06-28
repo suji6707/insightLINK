9835d14c862ab5a7512f8a6d5b470a40358117f6
Byeonggyu Shin <94808683+byeonggyu-shin@users.noreply.github.com>
2023-06-29 03:54:19 +0900

    fix: 빌드 에러 수정

--- client/src/features/Dashboard/components/AlarmModal.tsx
+++ client/src/features/Dashboard/components/AlarmModal.tsx
@@ -12,7 +12,7 @@ export default function AlarmModal() {
     <div className="w-[15rem] max-h-[20rem] min-h-[10rem] bg-slate-100 absolute z-20 top-10 rounded-3xl py-3">
       <div className="px-1 pb-2 text-xl border-b font-mediums">새로운 알림</div>
       <div className="border-b h-[2rem] pt-1" onClick={handleRedirect}>
-        "신병규"님이 새로운 스크린샷을 ...
+        `&#34;`신병규`&#34;` 님이 새로운 스크린샷을 ...
       </div>
     </div>
   );
