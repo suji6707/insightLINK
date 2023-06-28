4cea6031657c4c9daf1fc29911c86c208d4d4ec5
Byeonggyu Shin <94808683+byeonggyu-shin@users.noreply.github.com>
2023-06-29 03:45:22 +0900

    feat: 알람기능 레이아웃

---
+++ client/src/features/Dashboard/components/AlarmModal.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useRouter } from "next/router";
+
+export default function AlarmModal() {
+  const router = useRouter();
+
+  const handleRedirect = () => {
+    router.push("/social");
+  };
+
+  return (
+    <div className="w-[15rem] max-h-[20rem] min-h-[10rem] bg-slate-100 absolute z-20 top-10 rounded-3xl py-3">
+      <div className="px-1 pb-2 text-xl border-b font-mediums">새로운 알림</div>
+      <div className="border-b h-[2rem] pt-1" onClick={handleRedirect}>
+        "신병규"님이 새로운 스크린샷을 ...
+      </div>
+    </div>
+  );
+}
