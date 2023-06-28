d991a186cf2fdd31de1105beeb6ef5002c8a911a
suji <heojisu6707@gmail.com>
2023-06-28 21:20:48 +0900

    feat: 첫 가입자 소셜페이지

--- server/middlewares/recomUser.js
+++ server/middlewares/recomUser.js
@@ -4,6 +4,7 @@ import { recomUserQuery } from '../db/socialQueries.js';
 import { tagQuery } from '../db/socialQueries.js';
 import { profileQuery } from '../db/socialQueries.js';
 import { followCheckQuery } from '../db/followQueries.js';
+import { coldStartRecomQuery } from '../db/socialQueries.js';
 
 import { logger } from '../winston/logger.js';
 
@@ -15,9 +16,20 @@ export const recomUsersFunc = async (req, res, next) => {
   let connection = null;
   try {
     connection = await db.getConnection();
-    const [recommendList] = await connection.query(recomUserQuery(userId, 10));
-    req.recommendList = recommendList;  // attach to req
+    let [recommendList] = await connection.query(recomUserQuery(userId, 10));
     connection.release();
+
+    if (recommendList.length === 0) {
+      // console.log('fr: recommList is empty!');
+      if (req.path === '/user') {
+        await recomNewUsers(userId, res);
+      } else if (req.path === '/card') {
+        await recomNewCards(userId, res);
+      }
+      return;
+    }
+
+    req.recommendList = recommendList;  // attach to req
     next(); // move on to next middleware
   } catch (err) {
     connection?.release();
@@ -25,6 +37,107 @@ export const recomUsersFunc = async (req, res, next) => {
     res.status(500).send('Internal Server Error');
   }
 };
+
+/* recomUsersFunc()에서 호출 */
+// 이 두 함수는 각각의 recomUser, Cards에서 반환하는 결과값의 속성들을
+// 똑같이 로직을 줘야한다.
+export const recomNewUsers = async (userId, res) => {
+  let connection = null;
+  try {
+    connection = await db.getConnection();
+    const [recommendList] = await connection.query(coldStartRecomQuery);
+    /* recommendList는 가장 최근 생성된, 서로 다른 유저에 대한 파일 10개를 리턴한다 */
+    /* 리턴값은 두 개. file_id, user_id */
+
+    let data = [];
+    // 각 원소에 대해 유저 정보와 팔로우 여부를 불러온다.
+    for (const elem of recommendList) {
+      const recomUser = elem.user_id;
+      let tags = [];
+      const [tagList] = await connection.query(tagQuery, recomUser);
+      for (const item of tagList) {
+        tags.push(item.tag);
+      }
+
+      /* user profile - name, img */
+      const [profile] = await connection.query(profileQuery, recomUser);
+      const [checkFollow] = await connection.query(followCheckQuery, [userId, recomUser]);
+      const isFriend = checkFollow[0].count > 0 ? true : false;
+
+      const userProfile = {
+        userId: recomUser,
+        img: profile[0].profile_img,
+        userName: profile[0].user_name,
+        tags: tags,
+        isFriend: isFriend,
+      };
+
+      data.push(userProfile);
+    }
+    // console.log(data);
+    connection.release();
+    logger.info('/routes/social/recomUser 폴더, recomNewUsers 함수 get 성공 !');
+    res.status(200).send(data);  
+  } catch (err) {
+    connection?.release();
+    logger.error('/routes/social/recomUser 폴더, recomNewUsers 함수 get, err : ', err);
+    res.status(500).send('Internal Server Error');
+  }
+};
+
+
+
+export const recomNewCards = async (userId, res) => {
+  let connection = null;
+  try {
+    connection = await db.getConnection();
+    const [recommendList] = await connection.query(coldStartRecomQuery);
+    /* recommendList는 가장 최근 생성된, 서로 다른 유저에 대한 파일 10개를 리턴한다 */
+    /* 각 원소당 리턴값은 두 개. file_id, user_id */
+
+    let cardList = [];
+    // 각 원소당 카드 이미지/id와 함께 올린 유저 정보 리턴.
+    for (const elem of recommendList) {
+      const recomUser = elem.user_id;
+      const recomCard = elem.file_id;
+
+      let tags = [];
+      const [tagList] = await connection.query(tagQuery, recomUser);
+      for (const item of tagList) {
+        tags.push(item.tag);
+      }
+
+      const [profile] = await connection.query(profileQuery, recomUser);
+      const [checkFollow] = await connection.query(followCheckQuery, [userId, recomUser]);
+      const isFriend = checkFollow[0].count > 0 ? true : false;
+
+      const obj = {
+        userId: recomUser,
+        img: profile[0].profile_img,
+        userName: profile[0].user_name,
+        cardId: recomCard,
+        tag: tags,
+        cardImg: elem.img_url,      // elem[0]?
+        isFriend: isFriend,         // 실질적인 체크
+      };
+      cardList.push(obj);
+    }
+    console.log('fr:', cardList);
+    connection.release();
+    logger.info('/routes/social/recomCard 폴더 recomNewCards함수, get 성공 !');
+    res.status(200).send(cardList);
+  } catch (err) {
+    connection?.release();
+    logger.error(
+      '/routes/social/recomCard 폴더 recomNewCards함수, get, err : ',
+      err,
+    );
+    res.status(500).send('Internal Server Error');
+  }
+};
+
+
+
 
 
 /* 전역변수로 값 저장해서 불러오는 방법
@@ -34,7 +147,6 @@ export const recomUsersFunc = async (req, res, next) => {
   1) recomUsers의 인자, (여기선 상위 5개만.)
   2) recommendSimilarQuery의 인자로 넣자. 
 */
-// const recommendList = 
 
 export const recomUsers = async (req, res) => {
   const { user } = res.locals;
