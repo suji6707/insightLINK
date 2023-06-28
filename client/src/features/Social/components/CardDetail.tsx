885749bd5a732f03c5f22eaeff48d1ea682a572f
SeryeongK <99seryoung@naver.com>
2023-06-29 01:35:45 +0900

    feat: 소셜 상세 모달 디자인 수정 - #3

--- client/src/features/Social/components/CardDetail.tsx
+++ client/src/features/Social/components/CardDetail.tsx
@@ -5,6 +5,8 @@ import { AiFillPlusCircle, AiOutlinePlusCircle } from "react-icons/ai";
 
 import { CardDetail } from "@/types/social.types";
 import { POST } from "@/axios/POST";
+import { SocialImgModalAtom } from "@/recoil/atoms/SocialAtom";
+import { useRecoilState } from "recoil";
 
 const CardDetail = ({
   modalRef,
@@ -14,6 +16,7 @@ const CardDetail = ({
 }: CardDetail) => {
   const [detail, setDetail] = useState<any>();
   const [isPlus, setIsPlus] = useState(false);
+  const [showModal, setShowModal] = useRecoilState(SocialImgModalAtom);
 
   const getDetail = async () => {
     const token = getToken();
@@ -21,16 +24,19 @@ const CardDetail = ({
       `cards/info?cardId=${cardId}&userId=${userId}`,
       token
     );
-    // 에러 핸들링 코드 필요 🚨
-    if (data) {
-      setDetail(data);
+    if (data.status === 200) {
+      setDetail(data.data);
     }
   };
 
   const cloneCard = async () => {
     const token = getToken();
-    await POST(`social/clone`, { cardId: cardId }, token);
-    setIsPlus(true);
+    const data = await POST(`social/clone`, { cardId: cardId }, token);
+    console.log(data);
+    if (data.data === 200) {
+      setIsPlus(true);
+      setShowModal(false);
+    }
   };
 
   useEffect(() => {
@@ -45,30 +51,34 @@ const CardDetail = ({
     >
       <div className="absolute flex-col justify-start w-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white border top-1/2 left-1/2 h-4/6 drop-shadow-xl rounded-xl">
         <div className="flex flex-row justify-between p-2 p-4 border-b border-current">
-          <div className="flex flex-row">
-            <img
-              src={detail?.profile_img}
-              className="w-24 h-24 rounded-full"
-              alt="profile"
-            />
-            <p>{detail?.userName}</p>
-            {detail?.cardTag &&
-              detail?.cardTag.map((t: string, index: number) => {
-                return (
-                  <p key={index} className="p-2 m-2">
-                    #{t}
-                  </p>
-                );
-              })}
+          <div className="w-full flex flex-row items-center justify-between">
+            <div className="flex items-center">
+              <img
+                src={detail?.profile_img}
+                className="w-24 h-24 rounded-full border-[0.5rem] border-colorBlue"
+                alt="profile"
+              />
+              <p className="text-3xl font-semibold ml-5">{detail?.userName}</p>
+            </div>
+            <div className="flex">
+              {detail?.cardTag &&
+                detail?.cardTag.map((t: string, index: number) => {
+                  return (
+                    <p key={index} className="p-2 m-2 text-xl font-semibold">
+                      #{t}
+                    </p>
+                  );
+                })}
+            </div>
           </div>
         </div>
-        <div>
-          <p>{detail?.content}</p>
+        <div className="w-3/4 h-96 mx-auto flex flex-col justify-center items-center pt-5 overflow-y-auto pt-48">
           <img
             src={detail?.cardImage}
-            className="w-24 h-24 cursor-pointer"
+            className="w-full cursor-pointer"
             alt="screenshot"
           />
+          <p>{detail?.content}</p>
         </div>
         <div className="flex justify-center w-full">
           {isPlus ? (
