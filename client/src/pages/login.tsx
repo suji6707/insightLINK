6f6c6c5cd338a5fc7aae0e7e29a834773a1d8083
SeryeongK <99seryoung@naver.com>
2023-07-01 04:48:55 +0900

    feat: 회원가입, 로그인 디자인 수정 및 코드 정리

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,12 +1,9 @@
 import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 // recoil
 import { useSetRecoilState } from "recoil";
 import { LoginStateAtom } from "@/recoil/atoms/LoginStateAtom";
 
-import { useRouter } from "next/router";
-// Component
-import NavBar from "@/features/Dashboard/components/NavBar";
-import { Wrapper } from "@/styles/wrapper";
 import { POST } from "@/axios/POST";
 
 export default function Home() {
@@ -27,88 +24,70 @@ export default function Home() {
       false
     );
 
-    setUserEmail("");
-    setPassword("");
-
     if (response.data.success) {
-      alert(`로그인 성공!`);
+      alert(`다시 만나서 반가워요 : )`);
       const token = response.data.token;
-
       const userId = response.data.userId;
-      console.log("로그인한 userId : ", userId);
 
       setLoginId(userId);
 
-      // Store the token in local storage
       localStorage.setItem("token", token);
       localStorage.setItem("userId", userId);
 
       router.push("/dashboard");
     } else {
-      alert("fail!!");
+      alert("로그인 실패");
     }
   };
 
   useEffect(() => {
-    // Check if the user is already logged in
     const token = localStorage.getItem("token");
     if (token) {
-      // Redirect to the dashboard page
       router.push("/dashboard");
     }
   }, []);
 
   return (
-    <div>
-      <Wrapper>
-        <div className="flex flex-col items-center justify-center h-screen">
-          <div className="container flex flex-col items-end max-w-md mx-auto">
-            <div className="flex items-center justify-end mb-4">
-              <label
-                className="block mr-2 text-sm font-bold text-gray-700"
-                htmlFor="email"
-              >
-                이메일:
-              </label>
-              <div className="ml-auto input-box">
-                <input
-                  id="email"
-                  value={userEmail}
-                  onChange={(event) => setUserEmail(event.target.value)}
-                  placeholder="이메일"
-                  className="inline-block px-2 py-1 border border-gray-300 rounded form_input"
-                />
-              </div>
-            </div>
-            <div className="flex items-center justify-end mb-4">
-              <label
-                className="block mr-2 text-sm font-bold text-gray-700"
-                htmlFor="password"
-              >
-                비밀번호:
-              </label>
-              <div className="ml-auto input-box">
-                <input
-                  id="password"
-                  value={password}
-                  onChange={(event) => setPassword(event.target.value)}
-                  placeholder="비밀번호"
-                  type="password"
-                  className="inline-block px-2 py-1 border border-gray-300 rounded form_input"
-                />
-              </div>
-            </div>
-          </div>
-          <div className="flex justify-end mt-4">
-            <button
-              onClick={handleLogin}
-              className="px-4 py-2 mr-4 font-bold text-white bg-black rounded"
-            >
-              로그인
-            </button>
-          </div>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="container flex flex-col items-end max-w-md mx-auto">
+        <div className="flex items-center justify-end mb-4">
+          <label
+            className="block mr-2 text-sm font-bold text-gray-700"
+            htmlFor="email"
+          >
+            이메일:
+          </label>
+          <input
+            id="email"
+            value={userEmail}
+            onChange={(event) => setUserEmail(event.target.value)}
+            placeholder="이메일"
+            className="inline-block px-2 py-1 border border-gray-300 rounded form_input"
+          />
         </div>
-      </Wrapper>
+        <div className="flex items-center justify-end mb-4">
+          <label
+            className="block mr-2 text-sm font-bold text-gray-700"
+            htmlFor="password"
+          >
+            비밀번호:
+          </label>
+          <input
+            id="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            placeholder="비밀번호"
+            type="password"
+            className="inline-block px-2 py-1 border border-gray-300 rounded form_input"
+          />
+        </div>
+        <button
+          onClick={handleLogin}
+          className="px-[5rem] py-2 font-bold text-white bg-black rounded cursor-pointer"
+        >
+          로그인
+        </button>
+      </div>
     </div>
   );
 }
